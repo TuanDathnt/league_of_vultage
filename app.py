ccfeb5f33f335c0b7779cc777ecff34541466d0f
VerdantLevin <phamanhduc7025@gmail.com>
2023-10-29 16:40:20 +0700

    Initialized routes to pages

--- app.py
+++ app.py
@@ -1,12 +1,57 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,request,redirect,session,url_for
 
 app = Flask(__name__)
-
+app.secret_key = "tc,;sv.f;bmgnlmvlrmvlmwdmwsq".encode('utf8')
+app.template_folder = "templates"
+app.static_folder = "static"
 
 @app.route("/")
-def hello_world():
+def index():
   return render_template("index.html")
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+  pass
+
+@app.route("/logout")
+def logout():
+  pass
+
+@app.route("/register", methods = ['GET','POST'])
+def register():
+  pass
+
+@app.route("/browse", methods = ['GET','POST'])
+def show_products():
+  pass
+
+@app.route("/cart", methods = ['GET','POST'])
+def show_cart(account_id):
+  pass
+
+@app.route("/cart/finalize_purchase", methods = ['GET','POST'])
+def finalize_purchase(account_id):
+  pass
+
+@app.route("/profile/<account_id>", methods = ['GET','POST'])
+def profile(account_id):
+  pass
+
+@app.route("/profile/achievement/<account_id>", methods = ['GET','POST'])
+def show_achievement(account_id):
+  pass
+
+@app.route("/profile/history/<account_id>", methods = ['GET','POST'])
+def show_history(account_id):
+  pass
+
+@app.route("/library/<account_id>", methods = ['GET', 'POST'])
+def show_library(account_id):
+  pass
+
+@app.route("/read/<book_name>", methods = ['GET', 'POST'])
+def read(book_name):
+  pass
 
 
 if __name__=="__main__":
