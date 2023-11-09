6341f7b2d1eefd1c303fd7eab714a38a61fc95c1
VerdantLevin <phamanhduc7025@gmail.com>
2023-11-09 22:03:25 +0700

    Create route to Signup

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask,render_template,request,redirect,session,url_for
+import sqlite3
 
 app = Flask(__name__)
 app.secret_key = "tc,;sv.f;bmgnlmvlrmvlmwdmwsq".encode('utf8')
 app.template_folder = "templates"
 app.static_folder = "static"
+lovdb = 'db/lovdb.db'
 
 @app.route("/")
 def index():
@@ -13,18 +15,37 @@ def index():
 def login():
   if request.method=="GET":
     return render_template("login.html")
+  if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if check_exists(username,password):
+            session['username'] = username
+            return redirect(url_for("index"))
+
+def check_exists(username,password):
+    result = False
+    conn = sqlite3.connect(lovdb)
+    cursor = conn.cursor()
+    sqlcommand = f"select * from Account where Username ='{username}' and Password = '{password}'"
+    cursor.execute(sqlcommand)
+    data = cursor.fetchone()
+    if len(data) > 0:
+        result = True
+    conn.close()
+    return result
 
 @app.route("/logout")
 def logout():
-  pass
+  session.pop('username', None)
+  return redirect(url_for('index'))
 
 @app.route("/signup", methods = ['GET','POST'])
 def register():
   if request.method=="GET":
     return render_template("signup.html")
 
-@app.route("/browse", methods = ['GET','POST'])
-def show_products():
+@app.route("/browse/<category>/<subcategory>/<genre>/<age_range>/<sort_type>", methods = ['GET','POST'])
+def show_products(category, subcategory, genre, age_range, sort_type):
   pass
 
 @app.route("/cart", methods = ['GET','POST'])
@@ -51,8 +72,8 @@ def show_history(account_id):
 def show_library(account_id):
   pass
 
-@app.route("/read/<book_name>", methods = ['GET', 'POST'])
-def read(book_name):
+@app.route("/read/<book_id>", methods = ['GET', 'POST'])
+def read(book_id):
   pass
 
 
